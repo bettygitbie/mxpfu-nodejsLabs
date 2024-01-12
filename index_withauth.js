1b481ef04751f4cdd8fc53aa465b6ff4d29f71da
bettygitbie <107159318+bettygitbie@users.noreply.github.com>
2024-01-11 23:00:41 -0500

    Update index_withauth.js

--- index_withauth.js
+++ index_withauth.js
@@ -47,4 +47,25 @@ app.post("/login", (req,res) => {
     return res.status(200).send("User successfully logged in");
 });
 
+function getDateFromString(strDate) {
+    let [dd,mm,yyyy] = strDate.split('-')
+    return new Date(yyyy+"/"+mm+"/"+dd);
+}
+    
+// console.log(sorted_users);
+app.get("/sort",(req,res)=>{
+    let sorted_users=users.sort(function(a, b) {
+        let d1 = getDateFromString(a.DOB);
+        let d2 = getDateFromString(b.DOB);
+            return d1-d2;
+          });
+    res.send(sorted_users);
+});
+
+app.get("/lastname/:lastName", (req,res) => {
+    const lN = req.params.lastName;
+    let select_user = users.filter((user)=> user.lastName === lN);
+    res.send(select_user);
+})
+
 app.listen(PORT,()=>console.log("Server is running at port "+PORT));
